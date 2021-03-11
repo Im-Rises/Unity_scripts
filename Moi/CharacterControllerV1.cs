24f33f7bf6342a5221fc83800adfd41831b9a8bc
Im-Rises <quentin-pimbole@hotmail.fr>
2021-03-11 21:19:03 +0100

    Ajout script character controller

---
+++ Moi/CharacterControllerV1.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterControllerV1 : MonoBehaviour
+{
+    static int vitesseMarche = 10;
+    static int vitesseCourse = 30;
+
+    static int vitesseRotation = 100;
+
+    static float avancer;
+    static float rotation;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        avancer = Input.GetAxis("Vertical") * vitesseMarche * Time.deltaTime;
+        rotation = Input.GetAxis("Horizontal") * vitesseRotation * Time.deltaTime;
+
+        if (Input.GetKey("z"))
+            transform.Translate(0, 0, avancer);
+    }
+}
